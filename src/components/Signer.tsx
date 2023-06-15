72b07167b3b63dc50b0df99c51787767fd6069de
v1xingyue <qixingyue@gmail.com>
2023-06-15 18:22:41 +0800

    update signer

--- src/components/Signer.tsx
+++ src/components/Signer.tsx
@@ -1,22 +1,29 @@
 import { useWallet } from "@suiet/wallet-kit";
+import { fromSerializedSignature } from "@mysten/sui.js";
 import { useState } from "react";
 
 const Signer = () => {
+  const [result, updateResult] = useState({});
+  const [verifyResult, updateVerifyResult] = useState(false);
   const wallet = useWallet();
-  const [message, updateMessage] = useState("");
+  const [message, updateMessage] = useState("hello world");
   const signerAction = async () => {
     try {
       const msgBytes = new TextEncoder().encode(message);
       const result = await wallet.signMessage({
         message: msgBytes,
       });
-      const verifyResult = wallet.verifySignedMessage(result);
-      console.log("verify signedMessage", verifyResult);
-      if (!verifyResult) {
-        alert(`signMessage succeed, but verify signedMessage failed`);
-      } else {
-        alert(`signMessage succeed, and verify signedMessage succeed!`);
-      }
+      updateVerifyResult(wallet.verifySignedMessage(result));
+      const signature = fromSerializedSignature(result.signature);
+      console.log(signature);
+      updateResult({
+        signatureScheme: signature.signatureScheme,
+        pubKey: Buffer.from(signature.pubKey.toBytes()).toString("hex"),
+        signatureRaw: signature.signature.toString(),
+        signature: Buffer.from(signature.signature).toString("hex"),
+        signatureB64: Buffer.from(signature.signature).toString("base64"),
+        message: result.messageBytes,
+      });
     } catch (e) {
       console.error("signMessage failed", e);
       alert("signMessage failed (see response in the console)");
@@ -36,6 +43,10 @@ const Signer = () => {
       <button className="btn btn-primary ml-2" onClick={signerAction}>
         signMessage
       </button>
+      <div>
+        <p>{verifyResult ? "true" : "false"}</p>
+        <pre>{JSON.stringify(result, null, 2)}</pre>
+      </div>
     </div>
   );
 };
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,4 +1,4 @@
 export const SUI_PACKAGE =
-  "0xaf8dd5d5d2efd2e3c017f87190ed95f3bd92337fcfc730e0957056ebc9ad7986";
-export const GlobalID = "0xca35247041b850258b63617daa7375ccc3655bc93f59a2daf47221b9265a52e7";
+  "0x361d13981a88d230283aa6d842ebda30c9fa6427b9c14f2c5f3a92175039c54d";
+export const GlobalID = "0x5f1a9d0f9ac9a24edea91088f330827f4fbb22c297f65ebc7ff63c16cdbd86cf";
 export const NETWORK = "devnet";
